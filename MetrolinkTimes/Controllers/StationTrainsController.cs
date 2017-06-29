19be11333dd562ce86cfce8656c1d04d01081eb3
shubhamkulkarni01 <shubhamkulkarni01@gmail.com>
2017-06-28 22:11:47 -0700

    j

--- MetrolinkTimes/Controllers/StationTrainsController.cs
+++ MetrolinkTimes/Controllers/StationTrainsController.cs
@@ -126,7 +126,7 @@ namespace MetrolinkTimes.Controllers
             
             StationTrain stationTrain = new StationTrain { time = DateTime.Parse(updateData.time), station = db.Stations.First(i => i.Name == updateData.name), train = findClosest(updateData, db.StationTrains.Where(s=> s.station.Name == updateData.name).ToList())};
             if (stationTrain.train.train_id == 0 || stationTrain.train == null)
-            { return BadRequest(updateData.name+" "+updateData.time); }
+            { return BadRequest(updateData.name+" "+updateData.time+" "+stationTrain.station.Name+" "+stationTrain.train.train_id); }
             db.StationTrains.Add(stationTrain);
             await db.SaveChangesAsync();
 
